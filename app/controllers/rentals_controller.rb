928069782d4b857f464ac5aff5424710df9a959f
chutima puthachon <chutima.p8@gmail.com>
2024-03-05 10:17:46 +1100

    created controllers

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,2 +1,15 @@
 class RentalsController < ApplicationController
+  def show
+    @rental = Rental.find(params[:id])
+  end
+
+  def index
+    @rentals = Rental.all
+  end
+
+  private
+
+  def rental_params
+    params.require(:rental).permit(:status)
+  end
 end
