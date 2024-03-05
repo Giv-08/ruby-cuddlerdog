3a396e62ea6bcd9a2a391bea2fb1deeef8c1adf3
Debbie Kang <dhyk82@gmail.com>
2024-03-05 12:04:53 +1100

    dog html

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "pages#home"
+  resources :dogs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "dogs/:id", to: "dogs#show"
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "dogs", to: "dogs#index"
+  get "dogs/new", to: "dogs#new"
+  get "dogs/:id", to: "dogs#show"
   get "dogs/:id/edit", to: "dogs#edit"
+  post "dogs", to: "dogs#create"
   patch "dogs/:id", to: "dogs#update"
   # Defines the root path route ("/")
   delete '/dogs/:id', to: 'dogs#destroy'
-  get "dogs/new", to: "dogs#new"
-  post "dogs", to: "dogs#create"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,6 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# To delete all existing dogs listed
+Dog.destroy_all
