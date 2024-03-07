84eee431891f375e8293b4466d3e73b8ada93047
Debbie Kang <dhyk82@gmail.com>
2024-03-08 10:13:29 +1100

    index and show css updates

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,6 +1,10 @@
 class Dog < ApplicationRecord
   belongs_to :user
-  has_many :rentals
+  has_many :rentals, dependent: :destroy
 
   has_one_attached :photo
+  validates :users_username, presence: true
+  # if adding dog and owner photo
+  # has_one_attached :dog_photo
+  # has_one_attached :owner_photo
 end
