b7bbe3dd23f8b9cd6abe69c8551ac85b13440ea6
chutima puthachon <chutima.p8@gmail.com>
2024-03-30 18:56:49 +1100

    fixed cloudinary

---
+++ config/initializers/cloudinary.rb
@@ -0,0 +1,6 @@
+Cloudinary.config do |config|
+  config.cloud_name = 'dbv8jpvj5'
+  config.api_key = '954474967841987'
+  config.api_secret = 'CJjJ9M-X3FHZUzkTQc2BX1NdSFE'
+  config.cdn_subdomain = true
+end
