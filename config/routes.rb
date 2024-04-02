3e1419059e910484f4f574463abf65cc9ebf37f2
chutima puthachon <chutima.p8@gmail.com>
2024-04-02 20:05:57 +1100

    created contact

--- config/routes.rb
+++ config/routes.rb
@@ -33,5 +33,6 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   get "dashboard", to: "pages#dashboard", as: :dashboard
+  get "contact", to: "pages#contact", as: :contact
 
 end
