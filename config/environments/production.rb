96144caaefe58f5a5a31788e247489624f6bdaa6
chutima puthachon <chutima.p8@gmail.com>
2024-04-05 12:48:41 +1100

    changed config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,8 +38,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for Apache
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
-
-  config.secret_key_base = ENV['SECRET_KEY_BASE']
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :cloudinary
 
